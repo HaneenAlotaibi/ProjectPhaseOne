93eb408a65443df30a27e1fa94fce2187cf3df2d
Haneen Alotaibi <haneen.alotaibi@ccse.ku.edu.kw>
2022-11-28 19:26:31 +0300

    Initial commit

---
+++ src/com/Project/LockedMeApp.java
@@ -0,0 +1,49 @@
+package com.Project;
+import java.util.Scanner;
+
+public class LockedMeApp {
+	public static void main(String[] args) {		
+	    System.out.println("LockedMe.com");
+	    System.out.println( "Name :Haneen Alotaibi"); 
+	    Scanner in= new Scanner(System.in);
+	
+		boolean main=true;
+		int choice;
+while(true) {
+System.out.println(" Welcome to the LockedMe.com");
+System.out.println("Available Choices :");
+System.out.println(" 1 : Display names of all files(Ascending Order) ");
+System.out.println(" 2 : Menu ");
+System.out.println(" 3 : Exit ");
+System.out.println("Please enter the choice number:");
+
+ choice = in.nextInt();
+switch (choice)
+{
+case 1:
+
+	
+	break;
+	
+case 2:
+	
+	
+	
+	break;
+
+case 3:
+	
+	System.out.println("Exit from the Program");
+	 System.exit(0);
+	break;
+default:
+	 System.out.println("This is invalid Menu Choice! Please enter another choice number");
+
+break;
+
+}
+	}
+
+	}
+	}
+
