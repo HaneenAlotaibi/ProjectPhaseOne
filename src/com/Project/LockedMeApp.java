fc903aa985db6b29ef3216a6dd762ce2f9e073c0
Haneen Alotaibi <haneen.alotaibi@ccse.ku.edu.kw>
2022-11-30 18:47:11 +0300

    Edit lockedmeapp class

--- src/com/Project/LockedMeApp.java
+++ src/com/Project/LockedMeApp.java
@@ -10,46 +10,54 @@ public class LockedMeApp {
 		try (Scanner in = new Scanner(System.in)) {
 			int choice = 0;
 			while (true) {
-				System.out.println(" Welcome to the LockedMe.com");
-				System.out.println("Available Choices :");
-				System.out.println(" 1 : Display names of all files(Ascending Order) ");
-				System.out.println(" 2 : Menu ");
-				System.out.println(" 3 : Exit ");
-				System.out.println("Please enter the choice number:");
+				mainMenu();
 				try {
 					choice = in.nextInt();
 				} catch (InputMismatchException e) {
 					System.out.println("Mismatch Exception");
 					// e.printStackTrace();
 					LockedMeApp.main(null);
-					
-				}
-				switch (choice) {
-				case 1:
-
-					SortingFile.main(null);
-
-					break;
-
-				case 2:
-
-					FileMenu.main(null);
-
-					break;
-
-				case 3:
-
-					System.out.println("Exit from the Program");
-					System.exit(0);
-					break;
-				default:
-					System.out.println("This is invalid Menu Choice! Please enter another choice number");
-
-					break;
 
 				}
+				choiceFromMain(choice);
 			}
 		}
 
+	}
+
+	public static void mainMenu() {
+		System.out.println(" Welcome to the LockedMe.com");
+		System.out.println("Available Choices :");
+		System.out.println(" 1 : Display names of all files(Ascending Order) ");
+		System.out.println(" 2 : Menu ");
+		System.out.println(" 3 : Exit ");
+		System.out.println("Please enter the choice number:");
+	}
+
+	public static void choiceFromMain(int number) {
+		switch (number) {
+		case 1:
+
+			SortingFile.main(null);
+
+			break;
+
+		case 2:
+
+			FileMenu.main(null);
+
+			break;
+
+		case 3:
+
+			System.out.println("Exit from the Program");
+			System.exit(0);
+			break;
+		default:
+			System.out.println("This is invalid Menu Choice! Please enter another choice number");
+
+			break;
+
+		}
 	}
 }
