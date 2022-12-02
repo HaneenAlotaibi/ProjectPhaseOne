39b2b9230deb49107a4470e6c5d102e3031d76d6
Haneen Alotaibi <haneen.alotaibi@ccse.ku.edu.kw>
2022-12-02 12:49:48 +0300

    Put the files class as a function in one class

--- src/com/Project/FileMenu.java
+++ src/com/Project/FileMenu.java
@@ -1,6 +1,12 @@
 package com.Project;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class FileMenu {
@@ -8,7 +14,9 @@ public class FileMenu {
 		Scanner in = new Scanner(System.in);
 		int choice = 0;
 		while (true) {
-			System.out.println(" Menu");
+			System.out.println(" ***************************************");
+			System.out.println("                sub Menu                     ");
+			System.out.println(" ***************************************");
 			System.out.println("Available Choices :");
 			System.out.println(" 1 : Add file ");
 			System.out.println(" 2 : Delete file ");
@@ -27,25 +35,24 @@ public class FileMenu {
 			}
 			switch (choice) {
 			case 1:
-
-				CreateFile.main(null);
+				CreateFile(enter());
 
 				break;
 
 			case 2:
 
-				DeleteFile.main(null);
+				DeleteFile(enter());
 
 				break;
 			case 3:
 
-				SearchFile.main(null);
+				SearchFile(enter());
 				break;
 			case 4:
 
-				System.out.println("Exit from the menu");
+				System.out.println("Exit from the sub menu");
 
-				LockedMeApp.main(null);
+				LockedMeApp.themain();
 				break;
 			case 5:
 
@@ -57,6 +64,123 @@ public class FileMenu {
 
 				break;
 			}
+		}
+	}
+
+	public static void CreateFile(String name) {
+		try {
+			File myObj = new File(name);
+			// System.out.println(myObj.getPath());
+			if (myObj.createNewFile()) {
+
+				System.out.println("File created: " + myObj.getName());
+			} else {
+				System.out.println("File already exists.");
+			}
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+	}
+
+	public static void DeleteFile(String name) {
+		String[] flist = directory();
+		int flag = 0;
+		if (flist == null) {
+			System.out.println("Empty directory.");
+		} else {
+			for (int i = 0; i < flist.length; i++) {
+				String filename = flist[i];
+				if (filename.equals(name)) {
+					try {
+						File f = new File(name);
+						if (f.delete()) {
+							System.out.println(f.getName() + " deleted");
+							flag = 1;
+
+						} else {
+							System.out.println("failed");
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+
+			}
+			if (flag == 0) {
+				System.out.println(name+" Not Found");
+			}
+
+		}
+
+	}
+
+	public static String enter() {
+		System.out.println("Please enter file name");
+		Scanner scanner = new Scanner(System.in);
+		String filename = scanner.nextLine();
+		return filename;
+	}
+
+	public static void SearchFile(String name) {
+
+		String[] flist = directory();
+		int flag = 0;
+
+		if (flist == null) {
+			System.out.println("Empty directory.");
+		} else {
+
+			for (int i = 0; i < flist.length; i++) {
+				String filename = flist[i];
+				// System.out.println(flist[i]);
+				if (filename.equals(name)) {
+					System.out.println(filename + " found");
+					flag = 1;
+				}
+			}
+		}
+
+		if (flag == 0) {
+			System.out.println("File Not Found");
+		}
+
+	}
+
+	public static String[] directory() {
+		File directory = new File("C:\\Users\\lenovo\\KFHjava\\ProjectPhase"); // change the location files
+
+		String[] flist = directory.list();
+		return flist;
+	}
+
+	public static void sort() {
+
+		String[] flist = FileMenu.directory();
+		// Set<String> fileSet = new TreeSet<String>();
+		List<String> fileSet = new ArrayList<String>();
+
+		if (flist == null) {
+			System.out.println("Empty directory.");
+		} else {
+
+			for (int i = 0; i < flist.length; i++) {
+				// String filename = flist[i];
+				fileSet.add(flist[i]);
+
+			}
+			System.out.println("Sorted: ");
+			Collections.sort(fileSet, new Comparator<String>() {
+				@Override
+				public int compare(String o1, String o2) {
+
+					System.out.println(o1);
+					return o1.compareToIgnoreCase(o2);
+				}
+			});
+
+			// System.out.println("Sorted: " + fileSet);
+
 		}
 	}
 }
--- src/com/Project/LockedMeApp.java
+++ src/com/Project/LockedMeApp.java
@@ -5,8 +5,17 @@ import java.util.Scanner;
 
 public class LockedMeApp {
 	public static void main(String[] args) {
+		System.out.println(" ***************************************");
 		System.out.println("LockedMe.com");
 		System.out.println("Name :Haneen Alotaibi");
+		System.out.println(" Welcome to the LockedMe.com");
+		System.out.println(" ***************************************");
+
+		themain();
+
+	}
+
+	public static void themain() {
 		try (Scanner in = new Scanner(System.in)) {
 			int choice = 0;
 			while (true) {
@@ -16,20 +25,21 @@ public class LockedMeApp {
 				} catch (InputMismatchException e) {
 					System.out.println("Mismatch Exception");
 					// e.printStackTrace();
-					LockedMeApp.main(null);
+					themain();
 
 				}
 				choiceFromMain(choice);
 			}
 		}
-
 	}
 
 	public static void mainMenu() {
-		System.out.println(" Welcome to the LockedMe.com");
+		System.out.println(" ***************************************");
+		System.out.println("                  main menu             ");
+		System.out.println(" ***************************************");
 		System.out.println("Available Choices :");
 		System.out.println(" 1 : Display names of all files(Ascending Order) ");
-		System.out.println(" 2 : Menu ");
+		System.out.println(" 2 : sub Menu ");
 		System.out.println(" 3 : Exit ");
 		System.out.println("Please enter the choice number:");
 	}
@@ -38,13 +48,14 @@ public class LockedMeApp {
 		switch (number) {
 		case 1:
 
-			SortingFile.main(null);
+			FileMenu.sort();
 
 			break;
 
 		case 2:
 
 			FileMenu.main(null);
+			
 
 			break;
 
